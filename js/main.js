0f20efed11ab5382d1de0c7605aec83dc8044b35
aniMepsihh <112304266+aniMepsihh@users.noreply.github.com>
2025-02-20 22:23:24 +0500

    Добавлены jquery, адаптивы и убраны некоторые баги

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(window).load(function() {
+    $(".before-after").twentytwenty({
+        before_label: 'Без скинали',
+        after_label: 'Со скинали'
+    });
+    $('.before-slider').slick({
+        draggable: false,
+        dots: true,
+        dotsClass: 'before-slider__dots',
+        prevArrow: $('.arrow-left'),
+        nextArrow: $('.arrow-right')
+    })
+    $('.menu-button').on('click', function(){
+        $('.menu').toggleClass('menu_active');
+    })
+    $('.select__checked').on('click', function(){
+        $('.select__dropdown').toggleClass('select__dropdown_open')
+      });
+      $('.select__option').on('click',function(){
+        var value = $(this).attr('data-value');
+        $('#select-type').val(value);
+        $('.select__checked').text(value);
+        $('.select__dropdown').toggleClass('select__dropdown_open');
+      });
+      $("a[href^='#']").click(function(){
+        var _href = $(this).attr("href");
+        $("html,body").animate({scrollTop: $(_href).offset().top -130 + "px"});
+        return false
+      });
+});
